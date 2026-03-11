e30a29216c3c4a9ff67310c88bf60a3d68e13b95
ethan <notrealethan@gmail.com>
2026-03-11 17:08:07 -0500

    added new storyboards

---
+++ strata/strata/WorkoutLogViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorkoutLogViewController.swift
+//  strata
+//
+//  Created by Munoz, Ethan I on 3/11/26.
+//
+
+import UIKit
+
+class WorkoutLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

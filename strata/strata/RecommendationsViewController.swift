2dc92096842c79b5a8f1067a4d16ad0bfbaed4e6
Sanjana Madhav <sanjanamadhav31@gmail.com>
2026-04-03 19:59:19 -0500

    Issues with tab bar controller

--- strata/strata/RecommendationsViewController.swift
+++ strata/strata/RecommendationsViewController.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 
-let textCellIdentifier = "TextCell"
+let textCellIdentifier = "workoutCell"
 
 class RecommendationsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     var workouts: [Workout] = [
-           Workout(name: "Heavy Squat Workout", duration: "45 min", imageName: "squat"),
-           Workout(name: "Shoulder/Chest Workout", duration: "45 min", imageName: "chest"),
-           Workout(name: "Leg Cooldown Workout", duration: "30 min", imageName: "cooldown"),
-           Workout(name: "HIIT Workout", duration: "30 min", imageName: "hiit")
+           Workout(name: "Heavy Squat Workout", duration: "45 min", imageName: "squat", exercises: [
+            Exercise(name: "Barbell Squat", sets: 4, reps: 8, rest: "90 sec"),
+            Exercise(name: "Leg Press", sets: 3, reps: 10, rest: "60 sec")
+        ]),
+           Workout(name: "HIIT Workout", duration: "30 min", imageName: "hiit", exercises: [
+            Exercise(name: "Burpees", sets: 3, reps: 15, rest: "30 sec"),
+            Exercise(name: "Jump Squats", sets: 3, reps: 12, rest: "30 sec")
+        ])
        ]
     
     override func viewDidLoad() {
@@ -31,7 +35,22 @@ class RecommendationsViewController: UIViewController, UITableViewDataSource, UI
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath)
+            let workout = workouts[indexPath.row]
+            var content = cell.defaultContentConfiguration()
+            content.text = workout.name
+            content.secondaryText = workout.duration
+            content.image = UIImage(named: workout.imageName)
+            cell.contentConfiguration = content
+            return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showWorkoutDetail",
+           let destination = segue.destination as? WorkoutDetailViewController,
+           let indexPath = tableView.indexPathForSelectedRow {
+            destination.workout = workouts[indexPath.row]
+        }
     }
 
 }
---
+++ strata/strata/Workout.swift
@@ -0,0 +1,15 @@
+//
+//  Workout.swift
+//  strata
+//
+//  Created by Sanjana Madhav on 4/3/26.
+//
+
+import Foundation
+
+struct Workout {
+    let name: String
+    let duration: String
+    let imageName: String
+    let exercises: [Exercise]
+}
